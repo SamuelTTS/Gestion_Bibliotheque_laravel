c93d8106e0533bf508df91639c23ff6f2cc8a25c
Samuel_TTS <stchablintete@gmail.com>
2026-06-09 15:02:39 +0100

    fix: correction du namespace du middleware prometheus

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append('Iamfarhad\\Prometheus\\Middleware\\PrometheusMiddleware');
+        $middleware->append('Iamfarhad\\Prometheus\\Http\\Middleware\\PrometheusMetricsMiddleware');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
